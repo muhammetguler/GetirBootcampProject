01d99d467f9b305d23761bd0d32fc9bbe889bf47
Muhammet <gulermuhammet98@gmail.com>
2024-03-23 21:11:33 +0300

    last commit

--- swift/hello-world/Sources/HelloWorld/HelloWorld.swift
+++
@@ -1,3 +0,0 @@
-func hello() -> String {
-  return "Hello, World!"
-}
--- swift/hello-world/Tests/HelloWorldTests/HelloWorldTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-
-@testable import HelloWorld
-
-class HelloWorldTests: XCTestCase {
-  func testHello() {
-    XCTAssertEqual(hello(), "Hello, World!")
-  }
-}
